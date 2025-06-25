1d1b4e0bdd0516790a038bb3b1050f90f6341713
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-25 23:18:05 +0530

    test AMI

--- roboshop.sh
+++ roboshop.sh
@@ -11,9 +11,9 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c550dd13db8bcafc --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceID" --output text)
     if [ $instance != "frontend" ]
     then
-        IP=$(aws ec2 describe-instances --instance-ids i-0bfec147b3b4e7669 --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids i-0bfec147b3b4e7669 --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
-    echo "$instance IP address: $IP"
+    echo "$INSTANCE_ID: $IP"
 done
