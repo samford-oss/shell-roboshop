d3f37597a1b2396c6db61437e68237412ea8281a
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-25 23:36:08 +0530

    test AMI

--- roboshop.sh
+++ roboshop.sh
@@ -8,12 +8,7 @@ DOMAIN_NAME="samali.xyz"
 
 for instance in "${INSTANCES[@]}"
 do
-  INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type t2.micro \
-    --security-group-ids $SG_ID \
-    --query 'Instances[0].InstanceId' \
-    --output text)
+  INSTANCE_ID=$(aws ec2 run-instances --image-id $ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids $sg-0c550dd13db8bcafc --query 'Instances[0].InstanceId' --output text)
 
   echo "Launched instance $instance with ID $INSTANCE_ID. Waiting for it to be running..."
 
@@ -21,15 +16,9 @@ do
   aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
   if [ "$instance" != "frontend" ]; then
-    IP=$(aws ec2 describe-instances \
-      --instance-ids "$INSTANCE_ID" \
-      --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-      --output text)
+    IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
   else
-    IP=$(aws ec2 describe-instances \
-      --instance-ids "$INSTANCE_ID" \
-      --query 'Reservations[0].Instances[0].PublicIpAddress' \
-      --output text)
+    IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
   fi
 
   echo "$instance IP address: $IP"
