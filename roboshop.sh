02656cfda2c14d4fd4df13ec4351851ca7ec985c
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-25 12:04:51 +0530

    test AMI

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0c550dd13db8bcafc"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+
+ZONE_ID="Z07609232NOR2ZDHRYB5N"
+DOMAIN_NAME="samali.xyz"
+
+for instance in ${INSTANCES[@]}
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c550dd13db8bcafc --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceID" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text
+    else
+        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text
+    fi
+    echo "$instance IP address: $IP"
+done
