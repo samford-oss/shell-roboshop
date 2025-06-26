ec231dbf235d930b6c27d6b64b8c12b1824c4301
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-26 11:28:18 +0530

    test AMI

--- roboshop.sh
+++ roboshop.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0c550dd13db8bcafc"
+Security_GroupId="sg-0c550dd13db8bcafc"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z09956772F3AG5T9NT00Q"
-DOMAIN_NAME="samali.xyz"
+Zone_ID="Z09956772F3AG5T9NT00Q"
+Domain_Name="samali.xyz"
 
-#for instance in "${INSTANCES[@]}"
-for instance in $@
-do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c550dd13db8bcafc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceID" --output text)
-    if [ $instance != "frontend" ]
-    then
-        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+for instance in ${INSTANCES[@]}
+#for instance in $@; do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $Security_GroupId --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$Domain_Name"
     else
-        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$Domain_Name"
     fi
     echo "$instance IP address: $IP"
-done
