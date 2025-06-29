884cd63157ac92a5fdac3fa6d4ce475369cd0a67
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-29 22:01:40 +0530

    test AMI

--- mongodb.sh
+++ mongodb.sh
@@ -56,7 +56,5 @@ systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarting mongod"
 
 END_TIME=$(date +%s)
-
 TOTAL_TIME=$(($END_TIME - $START_TIME))
-
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
